721b25fe361fae34a4a91dc39b562e5c5b59bb27
Rudik <lexa.arsenal.rudik@gmail.com>
2023-02-24 12:54:45 +0300

    Добавили TaskSolution

---
+++ TaskSolution/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+
+
+string[] firstArray = new string[5] { "43", "11", "array", "python", "add" };
+string[] secondArray = new string[firstArray.Length];
+
+void FormingArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FormingArray(firstArray, secondArray);
+PrintArray(secondArray);
